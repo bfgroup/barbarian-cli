4aedd77299bf663ee1823c4eccbf438b8cd34763
Rene Rivera <grafikrobot@gmail.com>
2021-05-10 23:30:55 -0500

    Some refactoring, and addition of branch command.

--- barbarians/barbarian.py
+++ barbarians/barbarian.py
@@ -3,7 +3,7 @@
 # (See accompanying file LICENSE.txt or http://www.boost.org/LICENSE_1_0.txt)
 
 from argparse import ArgumentParser
-from os import environ, getcwd
+from os import environ, getcwd, chdir
 from shutil import rmtree, copytree
 from subprocess import run, PIPE
 import hashlib
@@ -41,11 +41,21 @@ class Barbarian(object):
             help="user/channel, Pkg/version@user/channel (if name and version are not declared in the conanfile.py) Pkg/version@ if user/channel is not relevant."
         )
 
+        # Create upload branch..
+        ap_branch = ap_sub.add_parser(
+            "branch",
+            help="Create, if needed, the 'barbarian' branch for uploading exported recipes to.")
+        ap_branch.add_argument(
+            "action",
+            help="What action to apply to the upload branch.",
+            choices=['create', 'push']
+        )
+
         self.args = ap.parse_args()
 
         if self.args.command:
             if hasattr(self, "command_"+self.args.command):
-                getattr(self, "command_"+self.args.command)()
+                getattr(self, "command_"+self.args.command)(self.args)
 
     def exec(self, command, input=None, capture_output=False, env={}):
         input = input.encode() if input else None
@@ -60,18 +70,17 @@ class Barbarian(object):
         result.check_returncode()
         return result
 
-    _recipe_dir = None
-
-    @property
-    def recipe_dir(self):
-        if not self._recipe_dir and hasattr(self.args, 'path'):
-            self._recipe_dir = os.path.abspath(self.args.path)
-        return self._recipe_dir
+    # Root dir, calculated from recipe dir.
 
     _root_dir = None
 
     @property
     def root_dir(self):
+        return self._root_dir
+
+    @root_dir.setter
+    def root_dir(self, args):
+        self.recipe_dir = args
         if not self._root_dir:
             dir = os.path.dirname(
                 self.recipe_dir) if self.recipe_dir else getcwd()
@@ -79,15 +88,35 @@ class Barbarian(object):
                 dir = os.path.dirname(dir)
             self._root_dir = dir
             print("[INFO] root_dir =", self._root_dir)
-        return self._root_dir
+
+    # Recipe dir, calculated from "path" arg.
+
+    _recipe_dir = None
+
+    @property
+    def recipe_dir(self):
+        return self._recipe_dir
+
+    @recipe_dir.setter
+    def recipe_dir(self, args):
+        if not self._recipe_dir and hasattr(args, "path"):
+            self._recipe_dir = os.path.abspath(args.path)
+            # print("[INFO] recipe_dir =", self._recipe_dir)
+
+    # Recipe name and version, as a list, calculated from conan inspection "reference" arg.
 
     _recipe_name_and_version = None
 
     @property
     def recipe_name_and_version(self):
+        return self._recipe_name_and_version
+
+    @recipe_name_and_version.setter
+    def recipe_name_and_version(self, args):
+        self.recipe_dir = args
+        self.root_dir = args
         if not self._recipe_name_and_version:
-            # Inspect information on the recipe.
-            recipe_nv = self.args.reference.split('@', 1)[0].split('/')
+            recipe_nv = args.reference.split('@', 1)[0].split('/')
             if self.recipe_dir:
                 env = {'CONAN_USER_HOME': self.root_dir}
                 recipe_n = self.exec(
@@ -105,79 +134,163 @@ class Barbarian(object):
                     recipe_n = recipe_nv[0]
                     recipe_v = recipe_nv[1]
             self._recipe_name_and_version = [recipe_n, recipe_v]
-        return self._recipe_name_and_version
+            # print("[INFO] recipe_name_and_version =", self._recipe_name_and_version)
+
+    # Recipe data dir where the export puts information. This is locally controlled to be relative to
+    # the root dir. And calculated from root dir and "self.recipe_name_and_version".
 
     _recipe_data_dir = None
 
     @property
     def recipe_data_dir(self):
+        return self._recipe_data_dir
+
+    @recipe_data_dir.setter
+    def recipe_data_dir(self, args):
+        self.root_dir = args
+        self.recipe_name_and_version = args
         if not self._recipe_data_dir:
             self._recipe_data_dir = os.path.join(
                 self.root_dir, ".conan", "data", self.recipe_name_and_version[0], self.recipe_name_and_version[1])
-        return self._recipe_data_dir
+            # print("[INFO] recipe_data_dir =", self._recipe_data_dir)
 
     _recipe_user_and_channel = None
 
+    # Recipe user and channel, calculated from "reference" arg.
+
     @property
     def recipe_user_and_channel(self):
+        return self._recipe_user_and_channel
+
+    @recipe_user_and_channel.setter
+    def recipe_user_and_channel(self, args):
         if not self._recipe_user_and_channel:
-            recipe_uc = self.args.reference.split('@', 1)[1].split('/')
+            recipe_uc = args.reference.split('@', 1)[1].split('/')
             recipe_u = recipe_uc[0] if recipe_uc and len(
                 recipe_uc) > 0 and len(recipe_uc[0]) > 0 else '_'
             recipe_c = recipe_uc[1] if recipe_uc and len(
                 recipe_uc) > 1 and len(recipe_uc[1]) > 0 else '_'
             self._recipe_user_and_channel = [recipe_u, recipe_c]
-        return self._recipe_user_and_channel
+            # print("[INFO] recipe_user_and_channel =", self._recipe_user_and_channel)
+
+    # Recipe export dir, where conan puts the exported recipe data, calculated from "self.recipe_data_dir"
+    # and "self.recipe_user_and_channel".
 
     _recipe_export_dir = None
 
     @property
     def recipe_export_dir(self):
+        return self._recipe_export_dir
+
+    @recipe_export_dir.setter
+    def recipe_export_dir(self, args):
+        self.recipe_data_dir = args
+        self.recipe_user_and_channel = args
         if not self._recipe_export_dir:
             self._recipe_export_dir = os.path.join(
                 self.recipe_data_dir, *self.recipe_user_and_channel)
-        return self._recipe_export_dir
+            # print("[INFO] recipe_export_dir =", self._recipe_export_dir)
 
-    def command_export(self):
-        print("[INFO] Exporting to %s" % (self.recipe_export_dir))
-        # Remove old data.
-        rmtree(self.recipe_data_dir, ignore_errors=True)
-        # Do the basic export.
-        env = {'CONAN_USER_HOME': self.root_dir}
-        self.exec(["conan", "export", self.args.path,
-                  self.args.reference], env=env)
+    # Recipe publish dir, is where we create the published/uploaded recipe data. Calculated from
+    # "self.root_dir" and "self.recipe_name_and_version".
 
     _recipe_publish_dir = None
 
     @property
     def recipe_publish_dir(self):
+        return self._recipe_publish_dir
+
+    @recipe_publish_dir.setter
+    def recipe_publish_dir(self, args):
+        self.root_dir = args
+        self.recipe_name_and_version = args
         if not self._recipe_publish_dir:
             self._recipe_publish_dir = os.path.join(
                 self.root_dir,
                 ".barbarian",
                 *self.recipe_name_and_version)
-        return self._recipe_publish_dir
+            # print("[INFO] recipe_publish_dir =", self._recipe_publish_dir)
+
+    # Recipe exported revision, which is the revision in the generated export data.
+    # Uses "self.recipe_export_dir".
 
     _recipe_exported_revision = None
 
     @property
     def recipe_exported_revision(self):
+        return self._recipe_exported_revision
+
+    @recipe_exported_revision.setter
+    def recipe_exported_revision(self, args):
+        self.recipe_export_dir = args
         if not self._recipe_exported_revision:
             with open(os.path.join(self.recipe_export_dir, "metadata.json"), "r") as f:
                 j = json.loads(f.read())
                 self._recipe_exported_revision = j['recipe']['revision']
-        return self._recipe_exported_revision
+            # print("[INFO] recipe_exported_revision =", self._recipe_exported_revision)
+
+    # Recipe revision specific publish dir.
 
     _recipe_revision_pub_dir = None
 
     @property
     def recipe_revision_pub_dir(self):
+        return self._recipe_revision_pub_dir
+
+    @recipe_revision_pub_dir.setter
+    def recipe_revision_pub_dir(self, args):
+        self.recipe_publish_dir = args
+        self.recipe_exported_revision = args
         if not self._recipe_revision_pub_dir:
             self._recipe_revision_pub_dir = os.path.join(
                 self.recipe_publish_dir, self.recipe_exported_revision)
-        return self._recipe_revision_pub_dir
+            # print("[INFO] recipe_revision_pub_dir =", self._recipe_revision_pub_dir)
 
-    def command_upload(self):
+    # Utilities..
+
+    def have_branch(self, branch):
+        return self.exec(["git", "branch", "--list", branch], capture_output=True).stdout.strip() == branch
+
+    def make_empty_branch(self, branch, message):
+        if not self.have_branch(branch):
+            cwd = getcwd()
+            self.exec(["git", "worktree", "add", "--quiet", "-b",
+                       branch+"-tmp", os.path.join(cwd, "."+branch+".tmp")])
+            chdir(os.path.join(cwd, "."+branch+".tmp"))
+            self.exec(["git", "checkout", "--quiet", "--orphan", branch])
+            self.exec(["git", "rm", "--quiet", "-rf", "."])
+            self.exec(["git", "commit", "--allow-empty", "-m", message])
+            self.exec(["git", "branch", "--quiet", "-D", branch+"-tmp"])
+            chdir(cwd)
+            self.exec(["git", "worktree", "remove",
+                      os.path.join(cwd, "."+branch+".tmp")])
+
+    def make_barbarian_branch(self):
+        if not self.have_branch("barbarian"):
+            print("[INFO] Creating 'barbarian' branch.")
+            self.make_empty_branch("barbarian", "Barbarian upload branch.")
+
+    # Commands..
+
+    def command_export(self, args):
+        # Compute state.
+        self.root_dir = args
+        self.recipe_data_dir = args
+        self.recipe_export_dir = args
+        # Info.
+        print("[INFO] Exporting to %s" % (self.recipe_export_dir))
+        # Remove old data.
+        rmtree(self.recipe_data_dir, ignore_errors=True)
+        # Do the basic export.
+        env = {'CONAN_USER_HOME': self.root_dir}
+        self.exec(["conan", "export", args.path, args.reference], env=env)
+
+    def command_upload(self, args):
+        # Compute state.
+        self.recipe_export_dir = args
+        self.recipe_exported_revision = args
+        self.recipe_publish_dir = args
+        self.recipe_revision_pub_dir = args
         print("[INFO] Uploading revision %s to %s" %
               (self.recipe_exported_revision, self.recipe_publish_dir))
         # Remove old data.
@@ -217,6 +330,9 @@ class Barbarian(object):
             'time': datetime.datetime.utcnow().isoformat()+"+0000"}
         with open(os.path.join(self.recipe_publish_dir, "latest.json"), "w", encoding="utf-8") as f:
             json.dump(latest, f)
+
+    def command_branch(self, args):
+        self.make_barbarian_branch()
 
 
 def main():
