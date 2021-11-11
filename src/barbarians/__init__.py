cf2fc94496526abc6756f572a1218c0e8325ffb8
Rene Rivera <grafikrobot@gmail.com>
2021-11-11 10:31:38 -0600

    Add missing copyright/license statements.

---
+++ src/barbarians/__init__.py
@@ -0,0 +1,3 @@
+# Copyright 2021 René Ferdinand Rivera Morell
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE.txt or http://www.boost.org/LICENSE_1_0.txt)
